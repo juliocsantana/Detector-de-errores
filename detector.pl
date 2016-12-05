20fbf55eb79c9fe8863cb4d07a45d13b894663c0
juliocsantana <gollosantana5@gmail.com>
2016-12-04 19:53:10 -0600

    Agregar problemas

---
+++ detector.pl
@@ -0,0 +1,13 @@
+problema('compresor').
+problema('conexion').
+problema('mantenimiento').
+
+problemade('no_enfria',compresor).
+problemade('ningun_sonido',compresor).
+problemade('compresor_defectuoso',compresor).
+
+problemade('no_enfria',mantenimiento).
+problemade('falta_gas',mantenimiento).
+
+problemade('no_enciende',conexion).
+problemade('conexiones_electricas_inadecuadas',conexion).
