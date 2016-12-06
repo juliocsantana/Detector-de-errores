58ecfa69ad4068bb9cd1584575d7b9127bb0604c
juliocsantana <gollosantana5@gmail.com>
2016-12-06 16:03:35 -0600

    Agregar Soluciones

--- Detector.pl
+++ Detector.pl
@@ -12,16 +12,16 @@ problemade('falta_gas',mantenimiento).
 problemade('no_enciende',conexion).
 problemade('conexiones_electricas_inadecuadas',conexion).
 
-solucion('encontrar_fuga_y_soldar','compresor').
-solucion('checar_cableado','compresor').
-solucion('verificar_termostato','compresor').
-solucion('verificar_capacitor','compresor').
-solucion('verificar_resistencias_de_bobinas','compresor').
+soluciones('encontrar_fuga_y_soldar','compresor').
+soluciones('checar_cableado','compresor').
+soluciones('verificar_termostato','compresor').
+soluciones('verificar_capacitor','compresor').
+soluciones('verificar_resistencias_de_bobinas','compresor').
 
-solucion('hacer_limpieza','mantenimiento').
-solucion('recargar_gas','mantenimiento').
-solucion('encontrar_fuga_y_soldar','mantenimiento').
+soluciones('hacer_limpieza','mantenimiento').
+soluciones('recargar_gas','mantenimiento').
+soluciones('encontrar_fuga_y_soldar','mantenimiento').
 
-solucion('revisar_enchufe','conexion').
-solucion('revisar_voltaje','conexion').
-solucion('verificar_medidas_de_frio_y_calor','conexion').
+soluciones('revisar_enchufe','conexion').
+soluciones('revisar_voltaje','conexion').
+soluciones('verificar_medidas_de_frio_y_calor','conexion').
